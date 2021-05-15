79f903e6d14f38c00121ca6e15b14f5e63d1fcc9
prototowb <prototowb@gmail.com>
2021-05-15 23:26:11 +0200

    added layout page

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cookieSession({
     keys: ['lkasld235j']
